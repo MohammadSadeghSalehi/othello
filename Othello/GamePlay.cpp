6c01d924bf77da081b015da41b78fed48bb5ec8a
MohammadSadeghSalehi <34940948+MohammadSadeghSalehi@users.noreply.github.com>
2018-01-09 17:34:02 +0330

    Add files via upload
    gamePlay UI

--- Othello/GamePlay.cpp
+++ Othello/GamePlay.cpp
@@ -2,17 +2,54 @@
 // Created by Mohammad Sadegh Salehi.
 //
 #include "GamePlay.h"
+#include <string>
 #include <QtCore/qcoreapplication.h>
 #include <QtWidgets/QApplication>
-
+using namespace std;
 
 
 GamePlay::GamePlay(int height, int width, QWidget *parent) {
     this -> setGeometry( width ,  height , width, height);
+    QPalette palette1 = palette();
+    palette1.setColor(QPalette::Background, Qt::darkBlue);
+    this->setAutoFillBackground(true);
+    this->setPalette(palette1);
 
+    str1=str1+cScore;
+    str2=str2+pScore;
+    cpuScore=new QLabel(str1,this);
+    cpuScore->setGeometry(50,10,120,40);
+    cpuScore->setStyleSheet("background-color: gray;border-radius:10px;");
+    cpuScore->setAlignment(Qt::AlignCenter);
+    playerScore=new QLabel(str2,this);
+    playerScore->setGeometry(330,10,120,40);
+    playerScore->setStyleSheet("background-color: gray;border-radius:10px;");
+    playerScore->setAlignment(Qt::AlignCenter);
+
+    for(int i=0;i<8;i++)
+    {
+        for(int j=0;j<8;j++)
+        {
+            piece[i][j]= new QPushButton(this);
+            piece[i][j]->setGeometry(50+(j*50),100+(50*i),50,50);
+            piece[i][j]->setStyleSheet("background-color: green;border: 0.5px solid black;");
+
+            plate[i][j]= new QLabel(this);
+            plate[i][j]->setGeometry(52+(j*50),102+(50*i),46,46);
+            plate[i][j]->setStyleSheet("background-color: rgba(0,0,0,0%);border-radius:25px;");
+        }
+    }
+    plate[3][3]->setStyleSheet("background-color: white;border-radius:23px;");
+    plate[4][3]->setStyleSheet("background-color: black;border-radius:23px;");
+    plate[3][4]->setStyleSheet("background-color: black;border-radius:23px;");
+    plate[4][4]->setStyleSheet("background-color: white;border-radius:23px;");
 }
 
 
 GamePlay::~GamePlay() {
+
+}
+
+void GamePlay::checkValid() {
 
 }
--- Othello/GamePlay.h
+++ Othello/GamePlay.h
@@ -5,9 +5,12 @@
 #ifndef OTHELLO_GAMEPLAY_H
 #define OTHELLO_GAMEPLAY_H
 #include <QWidget>
+#include <QtWidgets>
 #include <QtWidgets/qmainwindow.h>
 #include <QtWidgets/qpushbutton.h>
+#include <QtWidgets/qlabel.h>
 #include <QtWidgets/qtextedit.h>
+#include <cstring>
 
 using namespace std;
 
@@ -18,12 +21,17 @@ Q_OBJECT
 public:
     GamePlay(int height,int width,QWidget *parent=0);
     ~GamePlay();
+    void checkValid();
 public slots:
 
 
 private:
-
-
+    QLabel *plate[8][8];
+    QPushButton *piece[8][8];
+    QChar cScore='0',pScore='0';
+    QString str1= "cpu score: ",str2="player score: ";
+    QLabel *cpuScore;
+    QLabel *playerScore;
 };
 
 #endif //OTHELLO_GAMEPLAY_H
